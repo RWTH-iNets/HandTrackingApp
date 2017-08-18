3a9bf61d08fe8c9b543852b675ec082b3b0f321a
alegla <alexander.gladis@rwth-aachen.de>
2017-08-18 16:10:56 +0200

    Log some more stuff in classification script.

--- Sauron/sauron/logeventlist.py
+++ Sauron/sauron/logeventlist.py
@@ -43,6 +43,8 @@ class LogEventList:
         def _build_window(cur_start, cur_end, cur_events, cur_screen_on, cur_foreground_app, cur_power_connected, cur_daydream_active):
             return {
                 'events': cur_events,
+                'start': cur_start,
+                'end': cur_end,
                 'screen_on': cur_screen_on,
                 'foreground_app': cur_foreground_app,
                 'power_connected': cur_power_connected,
